c26b8e73153c5f6f12eb9879fa46a441a91bfda4
S0nnyyy <74193141+S0nnyyy@users.noreply.github.com>
2022-09-10 02:05:12 +0200

    Add files via upload

---
+++ sy_ls/fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+loadscreen 'html/index.html'
+
+loadscreen_cursor 'yes'
+
+files {
+
+    -- Loading HTML FILES --
+
+    'html/index.html',
+
+    -- Loading CSS STYLE FILES --
+    
+    'html/style.css',
+
+    -- Loading LOGO, ICONS, SONG, AND BACKGROUND VIDEO FILES --
+
+    'html/img/*.png',
+    'html/video/loadingvid.mp4',
+    'html/song/song.mp3',
+}
+
+
+
+
+
+
